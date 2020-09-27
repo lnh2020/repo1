6ff350f0887760f5c73f5bd17ab2e9295a503fb7
lnh <331137909@qq.com>
2020-09-27 21:25:02 +0800

    本地工程添加到git

---
+++ springboot_quick/src/main/java/cool/lnh/QuickCintroller.java
@@ -0,0 +1,14 @@
+package cool.lnh;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuickCintroller {
+    @RequestMapping("/quick")
+    @ResponseBody
+    public String quick(){
+        return "测试Springboot是否成功!";
+    }
+}
